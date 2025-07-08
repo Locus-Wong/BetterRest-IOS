c2edbd9138284e366058c36baabf415bbba8e792
TSZ WA WONG <twwong9@myseneca.ca>
2025-07-08 17:15:12 -0400

    Add project source code

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Locus Wong on 2025-07-07.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
